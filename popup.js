e937b90f10130f18e52098b5ea6ee2c845a12b81
Zimo Li <7163127+lzm0@users.noreply.github.com>
2023-11-02 16:34:41 -0400

    Initial commit

---
+++ popup.js
@@ -0,0 +1,29 @@
+const appendHeading = (text) => {
+  const heading = document.createElement("h2");
+  heading.textContent = text;
+  document.body.appendChild(heading);
+};
+
+const appendParagraph = (text) => {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = text;
+  document.body.appendChild(paragraph);
+};
+
+const copyTextToClipboard = (text) => {
+  const copyFrom = document.createElement("textarea");
+  copyFrom.textContent = text;
+  document.body.appendChild(copyFrom);
+  copyFrom.select();
+  document.execCommand("copy");
+  copyFrom.blur();
+  document.body.removeChild(copyFrom);
+};
+
+const tabs = await chrome.tabs.query({
+  highlighted: true,
+});
+
+copyTextToClipboard(tabs.map((tab) => tab.url).join("\n"));
+appendHeading("Copied!");
+appendParagraph(`${tabs.length} tabs selected`);
