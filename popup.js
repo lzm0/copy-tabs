11bf88476e7da25afc50e46ec8e0e90e1ceac87d
Zimo Li <7163127+lzm0@users.noreply.github.com>
2023-11-03 10:45:18 -0400

    Add paste tab feature

--- popup.js
+++ popup.js
@@ -1,24 +1,58 @@
+const message = document.getElementById("message");
+
 const appendHeading = (text) => {
   const heading = document.createElement("h2");
   heading.textContent = text;
-  document.body.appendChild(heading);
+  message.appendChild(heading);
 };
 
 const appendParagraph = (text) => {
   const paragraph = document.createElement("p");
   paragraph.textContent = text;
-  document.body.appendChild(paragraph);
+  message.appendChild(paragraph);
 };
 
+const clipboard = document.getElementById("clipboard");
+
 const copyTextToClipboard = (text) => {
-  const copyFrom = document.createElement("textarea");
-  copyFrom.textContent = text;
-  document.body.appendChild(copyFrom);
-  copyFrom.select();
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
   document.execCommand("copy");
-  copyFrom.blur();
-  document.body.removeChild(copyFrom);
+  document.body.removeChild(textarea);
 };
+
+const isValidUrl = (text) => {
+  try {
+    new URL(text);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
+const getUrlsFromClipboard = () => {
+  const textarea = document.createElement("textarea");
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("paste");
+  const text = textarea.value;
+  document.body.removeChild(textarea);
+  return text.split("\n").filter(isValidUrl);
+};
+
+const clipboardUrls = getUrlsFromClipboard();
+document.getElementById("tab-count").textContent = clipboardUrls.length;
+if (clipboardUrls.length !== 0) {
+  document.getElementById("paste").disabled = false;
+}
+
+document.getElementById("paste").addEventListener("click", () =>
+  clipboardUrls.forEach((url) => {
+    chrome.tabs.create({ url });
+  })
+);
 
 const tabs = await chrome.tabs.query({
   highlighted: true,
